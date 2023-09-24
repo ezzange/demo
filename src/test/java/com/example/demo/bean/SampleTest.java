332c52be4c7a6be660c5fa55effa622e13a000bd
ezzange <ezzang22@naver.com>
2023-09-24 11:31:23 +0900

    bean list test

--- src/test/java/com/example/demo/bean/SampleTest.java
+++ src/test/java/com/example/demo/bean/SampleTest.java
@@ -1,16 +1,34 @@
 package com.example.demo.bean;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootTest
 public class SampleTest {
+    public AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
     @Test
-    void test() {
-        //given
+    public void testAllBeanList() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean =ac.getBean(beanDefinitionName);
+            System.out.println("name = "+ beanDefinitionName + " | Object = " + bean);
+        }
 
-        //when
-
-        //then
     }
+    @Test
+    public void testAppBeanList(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanName);
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION){
+                System.out.println("내가 등록한 bean = " + beanName);
+            }
+        }
+    }
+
+
 }
+
